630f124a6d2138560aa2151d553579fc68c65836
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-17 11:53:46 +0100

    import xml into R datadoc

--- R/IEEE_API.r
+++ R/IEEE_API.r
@@ -6,5 +6,19 @@
 	
 		return abstract
 	}
+
+# return XML document
+
+
+# parse xml document into R dataframe
 	
+#load XML package
+load.package('XML')
+library('XML')
+
+#place xml doc into R dataframe datadoc
+doc <- xmlParse("search-result.xml")
+datadoc <- xmlToDataFrame(doc)
+
+
 	
