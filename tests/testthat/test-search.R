083f3089b69f3f1dba138505dc9d94f32cb6a31a
Karl Broman <kbroman@gmail.com>
2014-12-01 15:08:07 -0600

    Fix test-search; had assumed that articles would come back in a particular order

--- tests/testthat/test-search.R
+++ tests/testthat/test-search.R
@@ -12,48 +12,48 @@ test_that("IEEE_count and IEEE_search work in a simple case", {
     count <- IEEE_count(query)
     expect_equal(omit_attr(count), 4)
 
-    result <- IEEE_search(query)
+    result <- IEEE_search(query, sort_by="title")
     expect_equal(nrow(result), 4)
     expect_equal(attr(result, "totalfound"), 4)
 
     titles <- c("A fast method of generating digital random numbers",
-                "The design of wide-band recursive and nonrecursive digital differentiators",
                 "An approach to the approximation problem for nonrecursive digital filters",
-                "Synthetic voices for computers")
+                "Synthetic voices for computers",
+                "The design of wide-band recursive and nonrecursive digital differentiators")
     authors <- c("Rader, C.M.;  Rabiner, L.R.;  Schafer, R.W.",
-                 "Rabiner, L.;  Steiglitz, K.",
                  "Rabiner, L.;  Gold, B.;  McGonegal, C.",
-                 "Flanagan, J.L.;  Coker, C.;  Rabiner, L.;  Schafer, R.W.;  Umeda, N.")
+                 "Flanagan, J.L.;  Coker, C.;  Rabiner, L.;  Schafer, R.W.;  Umeda, N.",
+                 "Rabiner, L.;  Steiglitz, K.")
     pubtitle <- c("Bell System Technical Journal, The",
                    "Audio and Electroacoustics, IEEE Transactions on",
-                   "Audio and Electroacoustics, IEEE Transactions on",
-                   "Spectrum, IEEE")
+                   "Spectrum, IEEE",
+                   "Audio and Electroacoustics, IEEE Transactions on")
 
     expect_equal(result$title, titles)
     expect_equal(result$authors, authors)
     expect_equal(result$pubtitle, pubtitle)
-    expect_equal(result$punumber, c("6731005", "8337", "8337", "6"))
+    expect_equal(result$punumber, c("6731005", "8337", "6", "8337"))
     expect_equal(result$py, rep("1970", 4))
-    expect_equal(result$volume, c("49", "18", "18", "7"))
-    expect_equal(result$issue, c("9", "2", "2", "10"))
+    expect_equal(result$volume, c("49", "18", "7", "18"))
+    expect_equal(result$issue, c("9", "2", "10", "2"))
     expect_equal(result$part, rep("", 4))
-    expect_equal(result$spage, c("2303", "204", "83", "22"))
-    expect_equal(result$epage, c("2310", "209", "106", "45"))
-    arnum <- c("6772779", "1162090", "1162092", "5212992")
+    expect_equal(result$spage, c("2303", "83", "22", "204"))
+    expect_equal(result$epage, c("2310", "106", "45", "209"))
+    arnum <- c("6772779", "1162092", "5212992", "1162090")
     expect_equal(result$arnumber, arnum)
     expect_equal(result$publicationId, arnum)
     expect_equal(result$pubtype, rep("Journals & Magazines", 4))
     expect_equal(result$publisher, c("Alcatel-Lucent", "IEEE", "IEEE", "IEEE"))
-    expect_equal(result$issn, c("0005-8580", "0018-9278", "0018-9278", "0018-9235"))
+    expect_equal(result$issn, c("0005-8580", "0018-9278", "0018-9235", "0018-9278"))
     expect_equal(result$thesaurusterms, c("",
-                                          paste0("Delay|Digital filters|Digital simulation|Frequency response|",
-                                                 "Gold|Laboratories|Poles and zeros|Sampling methods|Telephony|",
-                                                 "Wideband"),
                                           paste0("Band pass filters|Chebyshev approximation|Digital filters|",
                                                  "Filtering theory|Frequency|IIR filters|Laboratories|",
                                                  "Low pass filters|Poles and zeros|Telephony"),
                                           paste0("Computer displays|Frequency|Humans|Information rates|",
                                                  "Natural languages|Resonance|Speech analysis|",
-                                                 "Speech synthesis|Telephony|Vocabulary")))
+                                                 "Speech synthesis|Telephony|Vocabulary"),
+                                          paste0("Delay|Digital filters|Digital simulation|Frequency response|",
+                                                 "Gold|Laboratories|Poles and zeros|Sampling methods|Telephony|",
+                                                 "Wideband")))
 
 })
